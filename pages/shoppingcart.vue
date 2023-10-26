688ae6910fe45a5f3175738eb186e426b87f819f
Jiho <yoho4613@gmail.com>
2023-10-27 01:15:46 +1300

    cartItem component

---
+++ pages/shoppingcart.vue
@@ -0,0 +1,45 @@
+<template>
+  <MainLayout>
+    <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div v-if="false" class="h-[500px] flex items-center justify-center">
+        <div class="pt-20">
+          <img class="mx-auto" width="250" src="/cart-empty.png" />
+          <div class="text-xl text-center mt-4">No items yet?</div>
+          <div v-if="true" class="flex text-center">
+            <NuxtLink to="/auth" class="bg-[#fd374f] w-full text-white text-[21px] font-semibold p-1.5 rounded-full mt-4">
+              Sign in
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+
+      <div v-else class="md:flex gap-4 justify-between mx-auto w-full">
+        <div class="md:w-[65%]">
+          <div class="bg-white rounded-lg p-4">
+            <div class="text-2xl font-bold mb-2">
+              Shopping Cart (0)
+            </div>
+          </div>
+
+          <div class="bg-[#feeeef] rounded-lg p-4 mt-4">
+            <div class="text-red-500 font-bold">Welcome Deal applicable on 1 item only</div>
+          </div>
+          <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+            <div v-for="product in products">
+              <CartItem :product="product" :selectedArray="selectedArray" @selectedRadio="selectedRadioFunc" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+
+const products = [
+  { id: 1, title: "Title 1", description: "This is a description", url: "https://picsum.photos/id/7/800/800", price: 9999 },
+  { id: 2, title: "Title 2", description: "This is a description", url: "https://picsum.photos/id/71/800/800", price: 9699 },
+]
+</script>

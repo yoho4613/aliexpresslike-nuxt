d147c015994ddbadf4b9db63d5ea70d95f6520ea
Jiho <yoho4613@gmail.com>
2023-11-03 18:29:38 +1300

    Address form fetch

--- pages/address.vue
+++ pages/address.vue
@@ -31,6 +31,7 @@
 import MainLayout from '~/layouts/MainLayout.vue';
 import { useUserStore } from '~/stores/user';
 const userStore = useUserStore()
+const user = useSupabaseUser()
 
 let contactName = ref(null)
 let address = ref(null)
@@ -43,7 +44,16 @@ let isUpdate = ref(false)
 let isWorking = ref(false)
 let error = ref(null)
 
-watchEffect(() => {
+watchEffect(async () => {
+  currentAddress.value = await useFetch(`/api/prisma/get-address-by-user/${user.value.id}`)
+  if (currentAddress.value.data) {
+    contactName.value = currentAddress.value.data.name
+    address.value = currentAddress.value.data.address
+    zipCode.value = currentAddress.value.data.zipcode
+    city.value = currentAddress.value.data.city
+    country.value = currentAddress.value.data.country
+
+  }
   userStore.isLoading = false
 })
 
@@ -79,8 +89,36 @@ const submit = async () => {
     return
   }
 
-  // More Here...
+  if (isUpdate.value) {
+    await useFetch(`/api/prisma/update-address/${currentAddress.value.data.id}`, {
+      method: "PATCH",
+      body: {
+        userId: user.value.id,
+        name: contactName.value,
+        address: address.value,
+        zipCode: zipCode.value,
+        city: city.value,
+        country: country.value,
+      }
+    })
+    isWorking.value = false
 
+    return navigateTo('/checkout')
+  }
+
+  await useFetch(`/api/prisma/add-address/`, {
+    method: "POST",
+    userId: user.value.id,
+    name: contactName.value,
+    address: address.value,
+    zipCode: zipCode.value,
+    city: city.value,
+    country: country.value,
+  })
+
+  isWorking.value = false
+
+  return navigateTo('/checkout')
 }
 
 </script>
